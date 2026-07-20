649818dcaf03bb23d3ed95512518d34b2ef373d6
Ayaan Kumar <113128926+s-ayakumar@users.noreply.github.com>
2026-07-21 05:28:42 +0530

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-1

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-1.java
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Set<Node> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = p.parent;
+        }
+
+        while (q != null) {
+            if (ancestors.contains(q)) {
+                return q;
+            }
+            q = q.parent;
+        }
+
+        return null;
+    }
+}
