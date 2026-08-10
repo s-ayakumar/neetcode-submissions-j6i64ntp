3769c0ce15dc4b46bcab9c83c9a4f4f22b4f4a6b
Ayaan Kumar <113128926+s-ayakumar@users.noreply.github.com>
2026-08-10 10:58:37 +0530

    Add: find-target-in-rotated-sorted-array - submission-19

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-19.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // [1, 2, 3, 4, 5, 6] -> [3, 4, 5, 6, 1, 2] -> 4 (target = 1)
+        // unique elements (no duplicates)
+        // if a target does not exist return -1
+
+
+        // compare start, mid, and end. if 
+        // check val at pivot 
+        // search on left and right of pivot with helper
+
+        return searchHelper(nums, target, 0, nums.length - 1);
+    }
+
+    private int searchHelper (int[] nums, int target, int start, int end) {
+        if (start > end) return -1;
+
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (nums[start] <= nums[mid]) {
+            if (target < nums[mid] && target >= nums[start]) {
+                return searchHelper(nums, target, start, mid - 1);
+            }
+            else {
+                return searchHelper(nums, target, mid + 1, end);
+            }
+        }
+        else {
+            if (target > nums[mid] && target <= nums[end]) {
+                return searchHelper(nums, target, mid + 1, end);
+            }
+            else {
+                return searchHelper(nums, target, start, mid - 1);
+            }
+        }
+
+    }
+
+
+
+
+}
