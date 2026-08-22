a9ae7557aa851243b303864b5af7f9f393332701
Ayaan Kumar <113128926+s-ayakumar@users.noreply.github.com>
2026-08-22 10:02:22 -0700

    Add: coin-change - submission-41

---
+++ Data Structures & Algorithms/coin-change/submission-41.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for (int i = 1; i < dp.length; i++) {
+
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] > i) continue;
+
+                if (dp[i - coins[j]] == Integer.MAX_VALUE) {
+                    continue;
+                }
+
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+
+        if (dp[amount] == Integer.MAX_VALUE) return -1;
+
+        return dp[amount];
+
+
+    }
+}
