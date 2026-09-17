877255688221bb155cdc81b11e9d1d0539e03996
Ayaan Kumar <113128926+s-ayakumar@users.noreply.github.com>
2026-09-17 15:16:06 -0700

    Add: three-integer-sum - submission-40

---
+++ Data Structures & Algorithms/three-integer-sum/submission-40.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                }
+                else if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                }
+                else {
+                    List<Integer> curr = new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[left]);
+                    curr.add(nums[right]);
+                    output.add(curr);
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) { left++; }
+                    while (left < right && nums[right] == nums[right + 1]) { right--; }
+
+                }
+            }
+        }
+
+        return output;
+
+    }
+}
