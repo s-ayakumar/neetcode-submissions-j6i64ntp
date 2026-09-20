d3f63305416abf17c569689533bc44150b756785
Ayaan Kumar <113128926+s-ayakumar@users.noreply.github.com>
2026-09-19 19:28:08 -0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+    
+
+    private int binarySearch(int[] nums, int target, int start, int end) {
+        if (end < start) return -1;
+
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (target < nums[mid]) {
+            return binarySearch(nums, target, start, mid - 1);
+        }
+        else {
+            return binarySearch(nums, target, mid + 1, end);
+        }
+    }
+
+}
